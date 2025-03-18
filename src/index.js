50697df43c62371054a1a8f1deae4be8e5c8a488
Mohd Ayaan <mohdayaan8071@gmail.com>
2025-03-18 21:03:34 +0530

    first Commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const { port } = require('./config/serverConfig');
+//const ApiRoutes = require('./routes/index');
+
+const db = require('./models/index');
+
+const setupAndStartServer = async () => {
+    const app = express();
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+   // app.use('/api', ApiRoutes);
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+        if (process.env.SYNC_DB) {
+            db.sequelize.sync({ alter: true });
+        }
+    });
+}
+
+setupAndStartServer();
