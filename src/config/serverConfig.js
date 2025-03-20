212a85faf281b13be18a47199c52532687793b7e
Mohd Ayaan <mohdayaan8071@gmail.com>
2025-03-21 00:53:03 +0530

    ayaan

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -4,5 +4,8 @@ dotenv.config();
 module.exports = {
     port: process.env.PORT,
     EMAIL_ID : process.env.EMAIL_ID,
-    EMAIL_PASS : process.env.EMAIL_PASS
+    EMAIL_PASS : process.env.EMAIL_PASS,
+    EXCHANGE_NAME : process.env.EXCHANGE_NAME,
+    REMAINDER_BINDING_KEY : process.env.REMAINDER_BINDING_KEY,
+    MESSAGE_BROKER_URL : process.env.MESSAGE_BROKER_URL
 };
--- src/index.js
+++ src/index.js
@@ -5,12 +5,15 @@ const { port } = require('./config/serverConfig');
 //const {sendBasicEmail} = require('./services/email-service');
 const setUpJobs = require('./utils/job');
 const TicketController = require('./controllers/ticket-controller');
+const {createChannel} = require('./utils/message-queue');
 
 const setupAndStartServer = async () => {
     const app = express();
 
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
+
+    const channel = await createChannel();
     app.post('/api/v1/tickets', TicketController.create);
 
     app.listen(port, () => {
